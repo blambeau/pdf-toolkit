1f33e15c80e1169403f9078355d5da00b2af3e86
Bernard Lambeau <blambeau@gmail.com>
2012-02-02 22:22:08 +0100

    Extract native methods to their own module

--- lib/pdf/toolkit.rb
+++ lib/pdf/toolkit.rb
@@ -62,6 +62,9 @@ class PDF::Toolkit
   # Raised when a .pdf file cannot be saved
   class FileNotSaved < Error; end
 
+  require File.expand_path('../toolkit/native', __FILE__)
+  extend Native
+
   class << self
 
     # Add an accessor for a key.  If the key is omitted, defaults to a
@@ -89,28 +92,6 @@ class PDF::Toolkit
       end
     end
 
-    # Invoke +pdftk+ with the given arguments, plus +dont_ask+.  If :mode or
-    # a block is given, IO::popen is called.  Otherwise, Kernel#system is
-    # used.
-    #
-    #   result = PDF::Toolkit.pdftk(*%w(foo.pdf bar.pdf cat output baz.pdf))
-    #   io = PDF::Toolkit.pdftk("foo.pdf","dump_data","output","-",:mode => 'r')
-    #   PDF::Toolkit.pdftk("foo.pdf","dump_data","output","-") { |io| io.read }
-    def pdftk(*args,&block)
-      options = args.last.is_a?(Hash) ? args.pop : {}
-      args << "dont_ask"
-      args << options
-      result = call_program(:pdftk,*args,&block)
-      return block_given? ? $?.success? : result
-    end
-
-    # Invoke +pdftotext+.  If +outfile+ is omitted, returns an +IO+ object for
-    # the output.
-    def pdftotext(file,outfile = nil,&block)
-      call_program(:pdftotext,file,
-        outfile||"-",:mode => (outfile ? nil : 'r'),&block)
-    end
-
     def info_accessors
       @info_accessors ||= Hash.new{|h,k|
         if h.has_key?(k.to_s.to_sym)
@@ -126,28 +107,6 @@ class PDF::Toolkit
     attr_accessor :default_permissions, :default_input_password
     attr_accessor :default_owner_password, :default_user_password
     protected :default_owner_password=, :default_user_password=
-
-    private
-
-    def call_program(*args,&block)
-      old_stream = nil
-      options = args.last.is_a?(Hash) ? args.pop : {}
-      options[:mode] ||= 'r' if block_given?
-      unless options[:silence_stderr] == false
-        old_stream = STDERR.dup
-        STDERR.reopen(RUBY_PLATFORM =~ /mswin/ ? 'NUL:' : '/dev/null')
-        STDERR.sync = true
-      end
-      if options[:mode]
-        command = (args.map{|arg| %{"#{arg.to_s.gsub('"','\\"')}"}}).join(" ")
-        retval = IO.popen(command,options[:mode],&block)
-        retval
-      else
-        system(*args)
-      end
-    ensure
-      STDERR.reopen(old_stream) if old_stream
-    end
 
     def camelize_key(key)
       if key.to_s.respond_to?(:camelize)
---
+++ lib/pdf/toolkit/native.rb
@@ -0,0 +1,49 @@
+class PDF::Toolkit
+  module Native
+
+    # Invoke +pdftk+ with the given arguments, plus +dont_ask+.  If :mode or
+    # a block is given, IO::popen is called.  Otherwise, Kernel#system is
+    # used.
+    #
+    #   result = PDF::Toolkit.pdftk(*%w(foo.pdf bar.pdf cat output baz.pdf))
+    #   io = PDF::Toolkit.pdftk("foo.pdf","dump_data","output","-",:mode => 'r')
+    #   PDF::Toolkit.pdftk("foo.pdf","dump_data","output","-") { |io| io.read }
+    def pdftk(*args,&block)
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      args << "dont_ask"
+      args << options
+      result = call_program(:pdftk,*args,&block)
+      return block_given? ? $?.success? : result
+    end
+
+    # Invoke +pdftotext+.  If +outfile+ is omitted, returns an +IO+ object for
+    # the output.
+    def pdftotext(file,outfile = nil,&block)
+      call_program(:pdftotext,file,
+        outfile||"-",:mode => (outfile ? nil : 'r'),&block)
+    end
+
+    private
+
+    def call_program(*args,&block)
+      old_stream = nil
+      options = args.last.is_a?(Hash) ? args.pop : {}
+      options[:mode] ||= 'r' if block_given?
+      unless options[:silence_stderr] == false
+        old_stream = STDERR.dup
+        STDERR.reopen(RUBY_PLATFORM =~ /mswin/ ? 'NUL:' : '/dev/null')
+        STDERR.sync = true
+      end
+      if options[:mode]
+        command = (args.map{|arg| %{"#{arg.to_s.gsub('"','\\"')}"}}).join(" ")
+        retval = IO.popen(command,options[:mode],&block)
+        retval
+      else
+        system(*args)
+      end
+    ensure
+      STDERR.reopen(old_stream) if old_stream
+    end
+
+  end # module Native
+end # class PDF::Toolkit
