8c80937ebd110fd98437a51d22f7bca84c2158ee
Bernard Lambeau <blambeau@gmail.com>
2012-02-02 21:10:13 +0100

    Avoid explicit require to rubygems

--- lib/pdf/toolkit.rb
+++ lib/pdf/toolkit.rb
@@ -18,8 +18,6 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-require 'rubygems' rescue nil
 require 'tempfile'
 require 'forwardable'
 require 'active_support/all'
